600ff4bc5c4242f5c4a14f542a12cf1eb585a3d9
gabrielbnetto <32345621+gabrielbnetto@users.noreply.github.com>
2017-10-11 20:54:17 -0300

    Update menu_filme.py

---
+++ GUI/menu_filme.py
@@ -0,0 +1,76 @@
+from logica import filme
+
+def imprimir_filme(filme):
+    print("Codigo do Filme: ",  filme[0])
+    print ("Título: ", filme[1])
+    print ("Gênero: ", filme[2])
+    print ("Ano: ", filme[3])
+
+def menu_adicionar():
+    print ("Adicionar Filme \n")
+    cod_filme = int(input("Codigo do Filme: "))
+    titulo = (input("Título: ")
+    genero = input("Gênero: ")
+    ano = int(input("Ano: "))
+    filme.adicionar_filme(cod_filme,titulo,genero,ano)
+
+def menu_listar():
+    print ("Listar Filmes \n")
+    filmes = filme.listar_filmes()
+    for f in filmes:
+        imprimir_filme(f)
+
+def menu_buscar():
+    print ("Buscar Filme por Codigo \n")
+    cod_filme = int(input("Codigo do Filme: "))
+    f = filme.buscar_filme(cod_filme)
+    if (f == None):
+        print ("Filme não encontrado")
+    else:
+        imprimir_filme(f)
+
+def menu_buscar_por_genero():
+    print ("Buscar Filme por Gênero \n")
+    genero = int(input("Gênero: "))
+    f = filme.buscar_filme_por_genero(genero)
+    if (f == None):
+        print ("Filme não encontrado")
+    else:
+        imprimir_filme(f)
+
+def menu_remover():
+    print ("Remover Filme \n")
+    cod_filme = int(input("Codigo do Filme: "))
+    f = filme.remover_filme(cod_filme)
+    if (f == False):
+        print ("Filme não encontrado")
+    else:
+        print ("Filme removido")
+
+def mostrar_menu():
+    rodar_filme = True
+    menu = ("Menu Filme\n"+
+            "(1) Adicionar novo Filme \n" +
+            "(2) Listar Filmes \n" +
+            "(3) Buscar Filme por Codigo \n" +
+            "(4) Buscar Filme por Genero \n" +
+            "(5) Remover Filme \n" +
+            "(0) Voltar\n"+
+            "----------------")
+
+    while(rodar_filme):
+        print (menu)
+        op = int(input("Digite sua escolha: "))
+
+        if (op == 1):
+            menu_adicionar()
+        elif(op == 2):
+            menu_listar()
+        elif(op == 3):       
+            menu_buscar()
+        elif (op == 4):
+            menu_buscar_por_genero()
+        elif (op == 5):
+            menu_remover()
+        elif (op == 0):
+            rodar_filme = False
