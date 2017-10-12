cc6413d25820021cbcae11f9777f0fa0f0f7f519
Fernando Ferreira Cunha <fernandofc16@gmail.com>
2017-10-11 22:24:43 -0300

    Atualização do histórico

--- logica/historico.py
+++ logica/historico.py
@@ -1,5 +1,5 @@
-from logica import filme
-from logica import usuario
+import filme
+#from logica import usuario
 
 filmes_vistos_usuarios = []
 
@@ -12,6 +12,24 @@ def registrar_filme_assistido(cpf, cod_filme):
 
 
 def listar_filmes_assistidos(cpf):
+    codigos_filmes = []
     for i in range(len(filmes_vistos_usuarios)):
         if filmes_vistos_usuarios[i][0] == cpf:
-            return filmes_vistos_usuarios[i][1:]
+            codigos_filmes = filmes_vistos_usuarios[i][1:]
+            break
+    filmes = []
+    for codigo in codigos_filmes:
+        f = filme.buscar_filme(codigo)
+        filmes.append(f[1])
+    return filmes
+            
+##registrar_filme_assistido(1234567, 555555)
+##registrar_filme_assistido(1234567, 444444)
+##
+##registrar_filme_assistido(1473343, 444444)
+##
+##registrar_filme_assistido(9743458, 444444)
+##
+##print(listar_filmes_assistidos(1234567))
+##print(listar_filmes_assistidos(1473343))
+##print(listar_filmes_assistidos(9743458))
