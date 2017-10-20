09d4dbd98cee4968ee39f08b9912ae981b06b6fc
gabrielbnetto <32345621+gabrielbnetto@users.noreply.github.com>
2017-10-20 20:06:03 -0200

    Adicionando Remover_todos_usuarios

--- logica/usuario.py
+++ logica/usuario.py
@@ -20,6 +20,10 @@ def remover_usuario(cpf):
             return True
     return False
 
+def remover_todos_usuarios():
+    global usuarios
+    usuarios =[]
+
 def iniciar_usuarios():
     adicionar_usuario(555555,"Joao","joao@hotmail.com","123456")
     adicionar_usuario(444444,"Lucas","lucas@hotmail.com","RNIEHO")
